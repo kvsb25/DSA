c9f75dee1737e14d021159682d287e1a70686781
Kovidh <kovidhvsbhati@gmail.com>
2025-10-30 12:07:17 +0530

    73. Set Matrix Zeroes Optimal approach

--- 73. Set matrix zeroes/73.set-matrix-zeroes.cpp
+++ 73. Set matrix zeroes/73.set-matrix-zeroes.cpp
@@ -1,29 +1,43 @@
 class Solution {
 public:
-    void setRowZero(vector<vector<int>>& matrix, int row){
-        for(int i = 0; i<matrix[0].size(); i++){
-            matrix[row][i] = 0;
-        }
-    }
-    void setColZero(vector<vector<int>>& matrix, int col){
-        for(int i = 0; i<matrix.size(); i++){
-            matrix[i][col] = 0;
-        }
-    }
     void setZeroes(vector<vector<int>>& matrix) {
+        bool col = false;
         int m = matrix.size();
         int n = matrix[0].size();
         vector<vector<int>> rec;
-        for(int i = 0; i<m; i++){
-            for(int j = 0; j<n; j++){
-                if(matrix[i][j]==0){
-                    rec.push_back({i,j});
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    if (j == 0) {
+                        col = true;
+                    } else {
+                        matrix[0][j] = 0;
+                    }
+                    matrix[i][0] = 0;
                 }
             }
         }
-        for(int i = 0; i < rec.size();i++){
-            setRowZero(matrix, rec[i][0]);
-            setColZero(matrix, rec[i][1]);
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] != 0) {
+                    if (!(matrix[0][j] && matrix[i][0])) {
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+        }
+
+        if(matrix[0][0] == 0){
+            for(int i = 0; i<n;i++){
+                matrix[0][i] = 0;
+            }
+        }
+
+        if(col){
+            for(int i = 0; i<m; i++){
+                matrix[i][0] = 0;
+            }
         }
     }
 };
