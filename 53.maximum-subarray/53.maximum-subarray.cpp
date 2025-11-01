215fa2dc1f49b73ad93bb95759e4108c6dc96013
Kovidh <kovidhvsbhati@gmail.com>
2025-11-01 11:35:57 +0530

    add notes - 53. Maximum Subarray

--- 53.maximum-subarray/53.maximum-subarray.cpp
+++ 53.maximum-subarray/53.maximum-subarray.cpp
@@ -1,11 +1,21 @@
-class Solution {
+class Solution
+{
 public:
-    int maxSubArray(vector<int>& nums) {
+    int maxSubArray(vector<int> &nums)
+    {
         int res = nums[0];
         int total = 0;
 
-        for (int n : nums) {
-            if (total < 0) {
+        // iterate through each of the elements of the input array
+        for (int n : nums)
+        {
+            // Logic: If the current sum (`total`) is negative,
+            // If the current running sum (`total`) is negative, carrying it forward only reduces the overall sum.
+            // Starting fresh at 'n' (by resetting total to 0) is always the better choice.
+            // Mathematically: Since (total < 0), then (total + n) < (0 + n).
+            // Therefore, we reset `total` to 0 to start a new, potentially optimal subarray from 'n'.
+            if (total < 0)
+            {
                 total = 0;
             }
 
@@ -13,6 +23,6 @@ public:
             res = max(res, total);
         }
 
-        return res;        
+        return res;
     }
 };
