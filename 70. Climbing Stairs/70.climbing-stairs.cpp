f420a03ebd18a65d9ee7b900cf2d7a0076680aad
Kovidh <kovidhvsbhati@gmail.com>
2026-02-08 15:23:26 +0530

    70. Climbing Stairs

---
+++ 70. Climbing Stairs/70.climbing-stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n+1, -1);
+        int prev2 = dp[0] = 0;
+        int prev1 = dp[1] = 1;
+
+        for(int i = 2; i <= n+1; i++){
+            int curr = prev1+prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
+    }
+};
