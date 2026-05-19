c9c1accc2f1fc85200ac63de9ad55b8cff5c3e5f
Kovidh <kovidhvsbhati@gmail.com>
2026-05-19 12:38:49 +0530

    285. Inorder successor in BST

---
+++ Binary Search Tree/Medium/285. Inorder successor in BST/285.inorder-successor-in-bst.cpp
@@ -0,0 +1,34 @@
+/*
+Definition for Node
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+    int inOrderSuccessor(Node *root, Node *k) {
+        int succ = -1;
+        
+        while(root != NULL){
+            if(root->data > k->data){
+                // if curr val is greater than key, set it as successor and look for a smaller val
+                succ = root->data;
+                root = root->left;
+            } else {
+                // if curr val is smaller than key then look for a larger value because successor is obviously greater than key
+                root = root->right;
+            }
+        }
+        
+        return succ;
+    }
+};
